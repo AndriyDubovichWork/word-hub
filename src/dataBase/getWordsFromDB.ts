e339610c863fa3ed012acb401deb201553068bbf
AndriyDubovichWork <86286088+AndriyDubovichWork@users.noreply.github.com>
2023-02-09 17:42:45 +0200

    fixed get word from db

--- src/dataBase/getWordsFromDB.ts
+++ src/dataBase/getWordsFromDB.ts
@@ -1,19 +1,21 @@
 import setWordsToDB from './setWordsToDB';
+import RemoveWordFromDB from './RemoveWordFromDB';
 import { wordT } from './types';
 import wordsMock from './wordsMock.json';
 const getWordsFromDB = (): wordT[] => {
 	// @ts-ignore
-	let words: wordT[] = JSON.parse(localStorage.getItem('words')).words;
+	let words: wordT[] = JSON.parse(localStorage.getItem('words'))?.words;
 
-	localStorage.removeItem('words');
 	if (!words) {
 		words = wordsMock.words;
 	}
-	if (!words[0].english.word && words.length > 1) {
-		words.shift();
-		setWordsToDB({ words });
-		getWordsFromDB();
-	}
+	words.map((word) => {
+		if (!word.english.word && words.length > 1) {
+			RemoveWordFromDB(words, word.english.word);
+			// @ts-ignore
+			words = getWordsFromDB();
+		}
+	});
 
 	return words;
 };
